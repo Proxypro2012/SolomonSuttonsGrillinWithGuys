ee4076b498d4b1790faa38304a9c3c4f0ae69853
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-04 09:34:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit as st
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 0.5])
 
+nav_pages = ["Home", "My Cart",]
+
 with r1col2:
   st.title("Solomon Suttons")
 with r2col2:
@@ -10,6 +12,8 @@ with r2col2:
 
 
 navbar = st.sidebar.title("Navigation")
+
+navbar.radio(nav_pages[0])
 
 
 
