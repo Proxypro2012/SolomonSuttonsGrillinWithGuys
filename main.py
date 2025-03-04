e34e2fa79efd5ac8c06a4be6eb862123c89c4880
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-04 09:26:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
-r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
+r2col1, r2col2, r2col3 = st.columns([1, 2, 0.5])
 
 with r1col2:
   st.title("Solomon Suttons")
